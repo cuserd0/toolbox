82260ad6238c58c9ba098d13eeeadaadf11d1afd
cuserd0 <42154366+cuserd0@users.noreply.github.com>
2018-10-29 13:12:19 -0600

    Update main.bat

--- main.bat
+++ main.bat
@@ -4,5 +4,13 @@ echo.
 echo 1. Notes
 echo 2. Commands
 echo 3. Tickets
+echo 4. Compile Google Search
 echo.
 set /P uinput="choose a number>"
+
+
+if %uinput%==4 (
+  echo you are in the Compile Google Search function
+  )
+
+  
